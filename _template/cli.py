2760ef3e52d4d50982630099dc4418f79bac06a1
ChrispyMC <ChrispyMC@users.noreply.github.com>
2020-06-29 18:15:51 -0400

    Edit Template.

--- _template/cli.py
+++ _template/cli.py
@@ -9,13 +9,15 @@ except ImportError:
 parser = argparse.ArgumentParser(description="A template for command-line interface & Tkinter GUI projects.",
   prog="_template", epilog="Extra information can be put here. (Like a list of functions, for example.)")
 
-parser.add_argument("-c" , "--command", help="Command Description.", default="Default value.")
+parser.add_argument("--version", action="version", version="Template 2020-06-29")
+parser.add_argument("-a" , "--argument", help="Argument Description.", default="Default value.")
 
 args = parser.parse_args()
 
 def run():
   #> Code for checking arguments goes here.
-  main()
+  print(args.command)
+  #main()
 
 if __name__ == "__main__":
   run()
